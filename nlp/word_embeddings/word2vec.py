87e23e3db31a4c58bf22e2439cc388876d2f383a
shirakiya <shirakiya.pv@gmail.com>
2017-04-30 19:23:38 +0900

    introduce gensim word2vec

---
+++ nlp/word_embeddings/word2vec.py
@@ -0,0 +1,37 @@
+import os
+from gensim.models import word2vec
+
+
+class Word2Vec(object):
+
+    default_params = {
+        'sg': 1,  # 0: CBOW, 1: skip-gram
+        'alpha': 0.025,
+        'min_alpha': 0.0001,
+        'size': 300,
+        'window': 5,
+        'min_count': 5,
+        'workers': os.cpu_count(),
+        'iter': 5,
+    }
+
+    def __init__(self):
+        self._model = None
+
+    def train(self, sentences, save_path, **params):
+        train_params = {}
+        for k, v in self.default_params.items():
+            train_params[k] = params.get(k, v)
+
+        self._model = word2vec.Word2Vec(sentences, **train_params)
+        self._model.save(save_path)
+
+    def load(self, load_path):
+        self._model = word2vec.Word2Vec.load(load_path)
+
+    def get_word_vector(self, word):
+        try:
+            vector = self._model.wv[word]
+        except KeyError:
+            raise KeyError("Word doesn't exists.")
+        return vector
---
+++ sample_word2vec.py
@@ -0,0 +1,46 @@
+import argparse
+from pprint import pprint
+
+from nlp.word_embeddings.word2vec import Word2Vec
+from nlp.preprocessing.tokenizer import Tokenizer
+
+
+def train(args):
+    with open(args.input_path, 'r') as f:
+        sentences = f.readlines()
+
+    w = Word2Vec()
+    w.train(sentences, args.model_path)
+
+
+def get_vector(args):
+    w = Word2Vec()
+    w.load(args.model_path)
+
+    t = Tokenizer()
+    result = {}
+    for word in t.wakati_baseform(args.text):
+        try:
+            result[word] = w.get_word_vector(word)
+        except KeyError:
+            result[word] = None
+
+    pprint(result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    sub_parser = parser.add_subparsers()
+
+    train_parser = sub_parser.add_parser('train')
+    train_parser.add_argument('input_path', type=str)
+    train_parser.add_argument('model_path', type=str)
+    train_parser.set_defaults(func=train)
+
+    vector_parser = sub_parser.add_parser('vector')
+    vector_parser.add_argument('model_path', type=str)
+    vector_parser.add_argument('text', type=str)
+    vector_parser.set_defaults(func=get_vector)
+
+    args = parser.parse_args()
+    args.func(args)
